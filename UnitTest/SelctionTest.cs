fe6ecba425955bdb739e2ff60b108ba0cc548de4
krizzmp <krizzmp@gmail.com>
2018-02-01 13:03:20 +0100

    Add project files.

---
+++ UnitTest/SelctionTest.cs
@@ -0,0 +1,25 @@
+﻿using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class SelctionTest
+    {
+        private readonly SelectionController selectionController = new SelectionController();
+        private readonly TestDataContainer testData = new TestDataContainer();
+
+        [TestMethod]
+        public void TestMethod_NoInputData()
+        {
+            selectionController.SelectWinners();
+        }
+
+        [TestMethod]
+        public void TestMethod_HappyPath()
+        {
+            testData.FillListContainer_HappyPath();
+            selectionController.SelectWinners();
+        }
+    }
+}
---
+++ UnitTest/TestDataContainer.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+
+namespace UnitTest
+{
+    public class TestDataContainer
+    {
+        private ListContainer listContainer = ListContainer.GetInstance();
+
+        public void FillListContainer_HappyPath()
+        {
+            //listContainer.routeNumberList;
+            //listContainer.contractorList;
+        }
+    }
+}
